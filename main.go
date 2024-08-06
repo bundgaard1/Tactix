095d99ef3855df3be5fa4c9777b53ffa5348038a
Mathias Jørgensen <mathiasbbjorgensen@gmail.com>
2024-08-06 17:26:11 +0200

    Standard main

--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"tactix/cli"
-	"tactix/engine"
 )
 
 func main() {
-
 	cli.Run()
-
-	pos := engine.FromFEN("2k5/K6r/8/2Pp4/8/8/8/8 w - d6 0 6")
-	fmt.Print(pos.String())
-	bb := engine.BBKingAttackedMask(&pos)
-	moves := engine.GetValidMoves(&pos)
-
-	for i := 0; i < moves.Count; i++ {
-		move := moves.Moves[i]
-		fmt.Println(move.String())
-	}
-
-	fmt.Print(bb.StringOnBoard())
-
 }
